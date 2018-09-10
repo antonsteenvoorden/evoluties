da4cfb4e138ff8416dae37b41e5781811a1262f2
Anton S <antons@live.nl>
2018-09-10 19:26:25 +0200

    init function YO

--- player45.java
+++ player45.java
@@ -3,6 +3,7 @@ import org.vu.contest.ContestEvaluation;
 
 import java.util.Random;
 import java.util.Properties;
+import java.util.Arrays;
 
 public class player45 implements ContestSubmission
 {
@@ -43,23 +44,57 @@ public class player45 implements ContestSubmission
             // Do sth else
         }
     }
-    
+
+	private static double getRandomNumberInRange(int min, int max) {
+
+		if (min >= max) {
+			throw new IllegalArgumentException("max must be greater than min");
+		}
+
+		Random r = new Random();
+		double randomValue = min + (max - min) * r.nextDouble();
+		return randomValue;
+	}
+
+    public double[][] init(int min, int max, int populationSize) {
+		double[][] parents = new double[populationSize][10];
+		for(int p = 0; p < populationSize; p++) {
+			double[] tmpParent = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+			for (int i = 0; i < 10; i++) {
+				tmpParent[i] = getRandomNumberInRange(min, max);
+			}
+			parents[p] = tmpParent;
+		}
+		return parents;
+	}
+
 	public void run()
 	{
 		// Run your algorithm here
-        
+        final int min = -5;
+		final int max = 5;
+		final int populationSize = 100;
+
         int evals = 0;
+
         // init population
+		double[][] population = init(min, max, 2);
+		System.out.println(population.length);
+		System.out.println(Arrays.toString(population[0]));
+		System.out.println(Arrays.toString(population[1]));
+
         // calculate fitness
         while(evals<evaluations_limit_){
             // Select parents
             // Apply crossover / mutation operators
-            double child[] = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+            double child[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
             // Check fitness of unknown fuction
             Double fitness = (double) evaluation_.evaluate(child);
+			System.out.println("fitness is");
+			System.out.println(fitness);
+			evals+=evaluations_limit_;
             evals++;
             // Select survivors
         }
-
 	}
 }
