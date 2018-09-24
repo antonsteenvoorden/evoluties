b2501c33b98676c02651c9e352bfa30cc74d90e3
DanielJAM <daniel.maaskant@gmail.com>
2018-09-24 11:01:55 +0200

    bij run.sh kan je nu typen welke je wilt runnen

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,28 @@
+
+#!/bin/bash
+
 javac -cp contest.jar player45.java Population.java CandidateSolution.java
 jar cmf MainClass.txt submission.jar player45.class Population.class CandidateSolution.class
-java -jar testrun.jar -submission=player45 -evaluation=SphereEvaluation -seed=1
+
+if [ "$1" = "Bent" ]; then
+    echo "\nBent Cigar"
+    java -jar testrun.jar -submission=player45 -evaluation=BentCigarFunction -seed=1
+fi
+
+if [ "$1" = "Sphere" ]; then
+    echo '\nSphere'
+    java -jar testrun.jar -submission=player45 -evaluation=SphereEvaluation -seed=1
+fi
+
+if [ "$1" = "Schaffers" ]; then
+    echo '\nSchaffers'
+    java -jar testrun.jar -submission=player45 -evaluation=SchaffersEvaluation -seed=1
+fi
+
+if [ "$1" = 'Katsuura' ]; then
+    echo '\nKatsuura (run time > 10s, wait)'
+    java -jar testrun.jar -submission=player45 -evaluation=KatsuuraEvaluation -seed=1
+fi
 
 # There are some security restrictions due to the online contest.
 # This makes it more difficult to store results.
