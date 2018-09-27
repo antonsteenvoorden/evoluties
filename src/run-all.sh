10cacb1c592753f57c5b8284c51442b7f26a5ede
PimDijt <pimdijt@gmail.com>
2018-09-27 11:37:29 +0200

    improved run scripts

--- src/run-all.sh
+++ src/run-all.sh
@@ -3,9 +3,9 @@ jar cmf MainClass.txt submission.jar player45.class CandidateSolution.class Matr
 echo 'Running all evaluations..'
 echo 'Bent Cigar'
 java -jar testrun.jar -submission=player45 -evaluation=BentCigarFunction -seed=1
-echo '\nSphere'
+echo 'Sphere'
 java -jar testrun.jar -submission=player45 -evaluation=SphereEvaluation -seed=1
-echo '\nSchaffers'
+echo 'Schaffers'
 java -jar testrun.jar -submission=player45 -evaluation=SchaffersEvaluation -seed=1
-echo '\nKatsuura (run time > 10s, wait)'
+echo 'Katsuura (run time > 10s, wait)'
 java -jar testrun.jar -submission=player45 -evaluation=KatsuuraEvaluation -seed=1
