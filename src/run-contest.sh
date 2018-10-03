ad697f0bb375dbdfca23aad24c0960fbee8486b5
DanielJAM <daniel.maaskant@gmail.com>
2018-10-03 10:50:40 +0200

    Added robustness and average of 30 runs different seeds

--- src/run-contest.sh
+++ src/run-contest.sh
@@ -1,3 +1,6 @@
+
+#!/usr/bin/env bash
+
 javac -cp contest.jar player45.java Population.java CandidateSolution.java
 jar cmf MainClass.txt submission.jar player45.class Population.class CandidateSolution.class
 java -jar testrun.jar -submission=player45 -evaluation=BentCigarFunction -seed=1
