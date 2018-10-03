6be2d4cb87d85e583b6196dbaa6fe184b5e526a7
DanielJAM <daniel.maaskant@gmail.com>
2018-10-03 14:02:56 +0200

    Zonder nullpointer in population

--- src/run-contest.sh
+++ src/run-contest.sh
@@ -3,6 +3,6 @@
 
 javac -cp contest.jar player45.java Population.java CandidateSolution.java
 jar cmf MainClass.txt submission.jar player45.class Population.class CandidateSolution.class
-java -Dpop_size=10.0 -Dn_par=2.0 -Dm_chance=1.4 -Dgs_dev=3.0 -jar testrun.jar -submission=player45 -evaluation=BentCigarFunction -seed=1
+java -Dpop_size=100.0 -Dn_par=5.0 -Dm_chance=0.1 -Dgs_dev=1.0 -jar testrun.jar -submission=player45 -evaluation=BentCigarFunction -seed=1
 
 #-Dvar1=0.5
