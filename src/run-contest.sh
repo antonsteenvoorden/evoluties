2c674b8984da0a98014dc1067c664cb10dd5f5cf
Anton S <antons@live.nl>
2018-10-10 13:50:12 +0200

    files needed to run

--- src/run-contest.sh
+++ src/run-contest.sh
@@ -3,6 +3,6 @@
 
 # L'Originale
 
-javac -cp contest.jar player45.java Population.java CandidateSolution.java ParentSelection.java SurvivorSelection.java
-jar cmf MainClass.txt submission.jar player45.class Population.class CandidateSolution.class ParentSelection.class SurvivorSelection.class
+javac -cp contest.jar player45.java Population.java CandidateSolution.java ParentSelection.java SurvivorSelection.java RecombinationOperator.java
+jar cmf MainClass.txt submission.jar player45.class Population.class CandidateSolution.class ParentSelection.class SurvivorSelection.class RecombinationOperator.class
 java -jar testrun.jar -submission=player45 -evaluation=BentCigarFunction -seed=1
